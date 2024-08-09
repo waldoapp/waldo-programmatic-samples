6e54bce3785afe50bba783e4b8fa46effaf45a11
Julien Roncaglia <j.roncaglia@tricentis.com>
2024-08-09 12:01:22 +0200

    Simplify the repository structure

---
+++ android/wdio.conf.ts
@@ -0,0 +1,31 @@
+import '@waldoapp/wdio-service';
+
+export const config: WebdriverIO.Config = {
+  autoCompileOpts: {
+    autoCompile: true,
+    tsNodeOpts: {
+      project: './tsconfig.json',
+      transpileOnly: true,
+    },
+  },
+  specs: ['./test/**/*.ts'],
+  capabilities: [{
+    platformName: 'Android',
+    'appium:app': 'wiki',
+    'waldo:automationName': 'Waldo',
+    'waldo:options': {
+      deviceName: 'Pixel 7',
+      osVersion: '33',
+    },
+  }],
+  logLevel: 'warn',
+  logLevels: {
+    webdriver: 'info',
+    '@waldoapp/wdio-service': 'info',
+  },
+  services: ['@waldoapp/wdio-service'],
+  reporters: ['spec'],
+  mochaOpts: {
+    timeout: 60000,
+  },
+};
